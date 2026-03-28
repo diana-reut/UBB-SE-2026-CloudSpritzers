322ec7db339845be96f605dac8a252da106b03aa
diana-reut <dianaradauti2013@gmail.com>
2026-03-28 02:06:50 +0200

    changed the Passenger class into the User class (Passenger was modified into User in order to follow the UML's rules) in the Ticket.cs file + added .slnx file to the WinUI project to make it runnable

--- CloudSpritzers1/src/model/ticket/Ticket.cs
+++ CloudSpritzers1/src/model/ticket/Ticket.cs
@@ -5,7 +5,7 @@ namespace CloudSpritzers.src.model.ticket
     public class Ticket
     {
         public int TicketId { get; }
-        public Passenger User { get; }
+        public User User { get; }
         public UrgencyLevelEnum UrgencyLevel { get; private set; }
         public StatusEnum Status { get; private set; }
         public TicketCategory Category { get; }
@@ -13,7 +13,7 @@ namespace CloudSpritzers.src.model.ticket
         public string Subject { get; }
         public string Description { get; }
         public DateTime CreatedAt { get; }
-        public Ticket(int ticketId, Passenger user, StatusEnum status, TicketCategory category, TicketSubcategory subcategory, string subject, string description, DateTime createdAt)
+        public Ticket(int ticketId, User user, StatusEnum status, TicketCategory category, TicketSubcategory subcategory, string subject, string description, DateTime createdAt)
         { 
             TicketId = ticketId;
             User = user;
